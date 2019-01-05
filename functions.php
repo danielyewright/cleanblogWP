58044dee0d8c376083067193124d9211bf93c964
Daniely Wright <danielywrighte@gmail.com>
2019-01-05 06:31:58 -0500

    Add editor styles and function helpers, #1

--- functions.php
+++ functions.php
@@ -80,11 +80,83 @@ if ( ! function_exists( 'bootstrapwp_setup' ) ) :
 			'flex-height' => true,
 		) );
 
-		/**
-		 * Add support for editor styles.
-		 */
+		// Add support for Block Styles.
+		add_theme_support( 'wp-block-styles' );
+
+		// Add support for full and wide align images.
+		add_theme_support( 'align-wide' );
+
+		// Add support for editor styles.
 		add_theme_support( 'editor-styles' );
-		add_editor_style( 'css/style-editor.css' );
+
+		// Enqueue editor styles.
+		add_editor_style( 'style-editor.css' );
+
+		// Add custom editor font sizes.
+		add_theme_support(
+			'editor-font-sizes',
+			array(
+				array(
+					'name'      => __( 'Small', 'bootstrapwp' ),
+					'shortName' => __( 'S', 'bootstrapwp' ),
+					'size'      => 19.5,
+					'slug'      => 'small',
+				),
+				array(
+					'name'      => __( 'Normal', 'bootstrapwp' ),
+					'shortName' => __( 'M', 'bootstrapwp' ),
+					'size'      => 22,
+					'slug'      => 'normal',
+				),
+				array(
+					'name'      => __( 'Large', 'bootstrapwp' ),
+					'shortName' => __( 'L', 'bootstrapwp' ),
+					'size'      => 36.5,
+					'slug'      => 'large',
+				),
+				array(
+					'name'      => __( 'Huge', 'bootstrapwp' ),
+					'shortName' => __( 'XL', 'bootstrapwp' ),
+					'size'      => 49.5,
+					'slug'      => 'huge',
+				),
+			)
+		);
+
+		// Editor color palette.
+		add_theme_support(
+			'editor-color-palette',
+			array(
+				array(
+					'name'  => __( 'Primary', 'bootstrapwp' ),
+					'slug'  => 'primary',
+					'color' => bootstrapwp_hsl_hex( 'default' === get_theme_mod( 'primary_color' ) ? 199 : get_theme_mod( 'primary_color_hue', 199 ), 100, 33 ),
+				),
+				array(
+					'name'  => __( 'Secondary', 'bootstrapwp' ),
+					'slug'  => 'secondary',
+					'color' => bootstrapwp_hsl_hex( 'default' === get_theme_mod( 'primary_color' ) ? 199 : get_theme_mod( 'primary_color_hue', 199 ), 100, 23 ),
+				),
+				array(
+					'name'  => __( 'Dark Gray', 'bootstrapwp' ),
+					'slug'  => 'dark-gray',
+					'color' => '#111',
+				),
+				array(
+					'name'  => __( 'Light Gray', 'bootstrapwp' ),
+					'slug'  => 'light-gray',
+					'color' => '#767676',
+				),
+				array(
+					'name'  => __( 'White', 'bootstrapwp' ),
+					'slug'  => 'white',
+					'color' => '#FFF',
+				),
+			)
+		);
+
+		// Add support for responsive embedded content.
+		add_theme_support( 'responsive-embeds' );
 	}
 endif;
 add_action( 'after_setup_theme', 'bootstrapwp_setup' );
@@ -126,15 +198,15 @@ add_action( 'widgets_init', 'bootstrapwp_widgets_init' );
  * Enqueue scripts and styles.
  */
 function bootstrapwp_scripts() {
-	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all' );
-	wp_enqueue_style( 'bootstrapwp-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'bootstrap-css', get_template_directory_uri() . '/css/bootstrap.min.css', array(), wp_get_theme()->get( 'Version' ), 'all' );
+	wp_enqueue_style( 'bootstrapwp-style', get_stylesheet_uri(), array(), wp_get_theme()->get( 'Version' ) );
+
+	wp_style_add_data( 'bootstrapwp-style', 'rtl', 'replace' );
 
 	wp_deregister_script( 'jquery' );
-	wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.3.1.slim.min.js', array(), '', true );
-	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ), '', true );
-	wp_enqueue_script( 'bootstrapwp-scripts', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '', true );
-	// wp_enqueue_script( 'bootstrapwp-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-	wp_enqueue_script( 'bootstrapwp-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_register_script( 'jquery', get_template_directory_uri() . '/js/jquery-3.3.1.slim.min.js', array(), wp_get_theme()->get( 'Version' ), true );
+	wp_enqueue_script( 'bootstrap-js', get_template_directory_uri() . '/js/bootstrap.bundle.min.js', array( 'jquery' ), wp_get_theme()->get( 'Version' ), true );
+	wp_enqueue_script( 'bootstrapwp-scripts', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), wp_get_theme()->get( 'Version' ), true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- inc/template-functions.php
+++ inc/template-functions.php
@@ -3,6 +3,7 @@
  * Functions which enhance the theme by hooking into WordPress
  *
  * @package Bootstrap_WP
+ * @since 1.0.0
  */
 
 /**
@@ -27,6 +28,16 @@ function bootstrapwp_body_classes( $classes ) {
 add_filter( 'body_class', 'bootstrapwp_body_classes' );
 
 /**
+ * Adds custom class to the array of posts classes.
+ */
+function bootstrapwp_post_classes( $classes, $class, $post_id ) {
+	$classes[] = 'entry';
+
+	return $classes;
+}
+add_filter( 'post_class', 'bootstrapwp_post_classes', 10, 3 );
+
+/**
  * Add a pingback url auto-discovery header for single posts, pages, or attachments.
  */
 function bootstrapwp_pingback_header() {
@@ -35,3 +46,84 @@ function bootstrapwp_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'bootstrapwp_pingback_header' );
+
+/**
+ * Convert HSL to HEX colors
+ */
+function bootstrapwp_hsl_hex( $h, $s, $l, $to_hex = true ) {
+
+	$h /= 360;
+	$s /= 100;
+	$l /= 100;
+
+	$r = $l;
+	$g = $l;
+	$b = $l;
+	$v = ( $l <= 0.5 ) ? ( $l * ( 1.0 + $s ) ) : ( $l + $s - $l * $s );
+	if ( $v > 0 ) {
+		$m;
+		$sv;
+		$sextant;
+		$fract;
+		$vsf;
+		$mid1;
+		$mid2;
+
+		$m = $l + $l - $v;
+		$sv = ( $v - $m ) / $v;
+		$h *= 6.0;
+		$sextant = floor( $h );
+		$fract = $h - $sextant;
+		$vsf = $v * $sv * $fract;
+		$mid1 = $m + $vsf;
+		$mid2 = $v - $vsf;
+
+		switch ( $sextant ) {
+			case 0:
+				$r = $v;
+				$g = $mid1;
+				$b = $m;
+				break;
+			case 1:
+				$r = $mid2;
+				$g = $v;
+				$b = $m;
+				break;
+			case 2:
+				$r = $m;
+				$g = $v;
+				$b = $mid1;
+				break;
+			case 3:
+				$r = $m;
+				$g = $mid2;
+				$b = $v;
+				break;
+			case 4:
+				$r = $mid1;
+				$g = $m;
+				$b = $v;
+				break;
+			case 5:
+				$r = $v;
+				$g = $m;
+				$b = $mid2;
+				break;
+		}
+	}
+	$r = round( $r * 255, 0 );
+	$g = round( $g * 255, 0 );
+	$b = round( $b * 255, 0 );
+
+	if ( $to_hex ) {
+
+		$r = ( $r < 15 ) ? '0' . dechex( $r ) : dechex( $r );
+		$g = ( $g < 15 ) ? '0' . dechex( $g ) : dechex( $g );
+		$b = ( $b < 15 ) ? '0' . dechex( $b ) : dechex( $b );
+
+		return "#$r$g$b";
+
+	}
+
+	return "rgb($r, $g, $b)";
+}
